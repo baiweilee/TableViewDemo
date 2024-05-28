58ddaece1433f4ca95c9b705632a582fd82be48a
kaiqianglee <kaiqianglee@qq.com>
2024-05-28 19:59:58 +0800

    提交Demo

---
+++ VVeboTableViewDemo/UIScreen+Additions.h
@@ -0,0 +1,18 @@
+//
+//  UIScreen+Additions.h
+//  Additions
+//
+//  Created by kaiqianglee on 13-6-15.
+//  Copyright (c) 2015年 kaiqianglee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIScreen (Additions)
+
++ (float)screenWidth;
++ (float)screenHeight;
++ (BOOL)isRetina;
++ (float)scale;
+
+@end
---
+++ VVeboTableViewDemo/VVeboTableView.h
@@ -0,0 +1,13 @@
+//
+//  VVeboTableView.h
+//  VVeboTableViewDemo
+//
+//  Created by kaiqianglee on 15/5/25.
+//  Copyright (c) 2015年 kaiqianglee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VVeboTableView : UITableView
+
+@end
